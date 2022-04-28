2203449673af042444a9cc8424735e65b0f4fc64
Andrew Guy <aag43@cam.ac.uk>
2022-04-28 20:20:42 +0100

    Added documentation, created new method for hybrid foreground segmentation

--- analysis.py
+++ analysis.py
@@ -1,4 +1,3 @@
-from pickle import TUPLE2
 from skimage.io import imread
 from skimage.color import rgb2gray
 from skimage.morphology import skeletonize, opening, closing, disk
@@ -90,9 +89,20 @@ class FilteredImage:
 
 
 class TubenessImage:
+    """
+    An image, filtered at multiple length scales for extracting statistics.
+    Approach is intended to reject spurious fibres where possible, so is very
+    conservative when segmenting foreground. Statistics should be interpreted
+    as the probability of a randomly selected point on a fibre in the top layer
+    having the given characteristic.
+    """
+    
     def __init__(self, image, sigmas,
                  equalize=False, normalize=True,
                  filter_method=None, filter_kwargs=None):
+        """
+        
+        """
         filter_method, filter_kwargs = FilteredImage.ensure_filter(
             filter_method, filter_kwargs)
         if equalize:
@@ -106,6 +116,11 @@ class TubenessImage:
         self.set_scores()
 
     def set_max_scales(self):
+        """
+        Generates the maximum response image (the traditional Frangi filter)
+        as well as recording the length scale which produced the value at each
+        pixel.
+        """
         self.max = np.zeros(self.image.shape)
         self.max_sigma = np.zeros(self.image.shape)
         for s, f in self.images.items():
@@ -115,7 +130,7 @@ class TubenessImage:
 
     def set_scores(self):
         """
-
+        Computes the scores returned by `get_scores`.
         """
         for s, f in self.images.items():
             f.response = np.sum(f.image * self.mask)
@@ -152,42 +167,64 @@ class TubenessImage:
 
     @staticmethod
     def get_threshold(image, method=None, kwargs=None):
+        """
+        Ensures that the thresholding method is valid, defaulting to Otsu's
+        method.
+        """
         if method is None:
             method = threshold_otsu
         if kwargs is None:
             kwargs = {}
         return method(image, **kwargs)
 
-    def get_filtered(self, idx):
+    def get_filtered(self, sigmas):
+        """
+        For the specified filter length scales, take the pointwise maximum.
+        """
         filtered = np.zeros(self.image.shape)
-        for s in idx:
+        for s in sigmas:
             filtered = np.maximum(filtered, self.images[s].image)
         return filtered
 
-    def set_skeleton(self, intensity_fraction=0.9, opening_radius=2,
-                     t_method=None, t_kwargs=None, s_method=None, report=False):
-        r = dict()
+    def get_foreground(self, intensity_fraction=0.9,
+                       t_method=None, t_kwargs=None):
+        """
+        Thresholds the output of Frangi's filter by taking the pointwise
+        maximum of filter images at length scales which are nearly as intense
+        as the length scale which contributed most intensely.
+        """
         intensity = np.array([f.contribution / f.dominance
                               for f in self.images.values()])
         sigmas = np.array(list(self.images.keys()))
         idx = np.nonzero(intensity >= intensity_fraction * np.amax(intensity))
         t_image = self.get_filtered(sigmas[idx[0]])
         threshold = TubenessImage.get_threshold(t_image, t_method, t_kwargs)
-        self.mask = self.max > threshold
-        if report:
-            r['thresholding'] = t_image
-            r['threshold'] = threshold
-            r['foreground'] = self.mask
+        return self.max > threshold
+
+    def set_skeleton(self, intensity_fraction=0.9, opening_radius=2,
+                     t_method=None, t_kwargs=None, s_method=None):
+        """
+        Takes the foreground of the image, removes spurious blobs and branches
+        by morphological opening, then skeletonizes the result.
+
+        Parameters:
+            intensity_fraction: float 
+                See `get_foreground`
+            opening_radius: int
+                The disk diameter used for opening
+            t_method: callable (ndarray -> float)
+                See `get_foreground`
+            t_kwargs: dict
+                See `get_foreground`
+            s_method: callable (ndarray -> ndarray)
+                Skeletonization method, defaults to `skeletonize`
+        """
+        self.mask = self.get_foreground(intensity_fraction, t_method, t_kwargs)
         if opening_radius != 0:
             self.mask = opening(self.mask, disk(opening_radius))
-            if report:
-                r['foreground_opened'] = self.mask
         if s_method is None:
             s_method = skeletonize
         self.mask = s_method(self.mask)
-        if report:
-            r['skeleton'] = self.mask
-            return r
 
     def get_diameter_count(self):
         masked = self.mask * self.max_sigma
@@ -248,8 +285,9 @@ class PoreImage:
                 threshold = threshold_otsu(self.tube.image)
             self.foreground = self.tube.image > threshold
         elif mode == 'h':
-            self.foreground = self.tube.image > threshold #threshold_otsu(self.tube.image)
-            #if threshold is None:
+            # threshold_otsu(self.tube.image)
+            self.foreground = self.tube.image > threshold
+            # if threshold is None:
             #    threshold = 0.9
             intensity = np.array([f.contribution / f.dominance
                                   for f in self.tube.images.values()])
@@ -262,7 +300,16 @@ class PoreImage:
         if close != 0:
             self.foreground = closing(self.foreground, disk(close))
 
-    def analyse(self):
+    def set_foreground(self, i_factor=0.9, c_radius=2, o_radius=2):
+        tf = self.tube.get_foreground(i_factor)
+        if c_radius != 0:
+            tf = closing(tf, disk(c_radius))
+        fg = self.tube.image > threshold_otsu(self.tube.image)
+        if o_radius != 0:
+            fg = opening(fg, disk(o_radius))
+        self.foreground = tf | fg
+
+    def analyze(self):
         self.labelled, self.max_label = label(
             ~self.foreground, return_num=True, connectivity=1)
         self.areas = [np.sum(self.labelled == k)
