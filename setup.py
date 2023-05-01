5f73cd062f81bba05f60eda06fde7e8aaf52a1b4
Andrew Guy <aag43@cam.ac.uk>
2023-05-01 13:12:22 +0100

    Modified setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-install_requires = [
-    'scikit-image',
-    'imagecodecs'
-]
+from setuptools import setup, find_packages
+
+setup(
+    name = "FibreAnalysis", 
+    version = "0.1", 
+    packages = find_packages(), 
+    install_requires = ['scikit-image', 
+                        'imagecodecs']
+    )
